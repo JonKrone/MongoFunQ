7fee0505b8d7a997130ca41b3d015666eb069ac5
MKS-students <mks.students@hackreactor.com>
2016-02-08 14:33:37 -0600

    Implement by function and its tests. I think I should refactor it to only work for a specific obj rather than an array of objs. When I need array functionality, we can just compose map

--- db/db_utils.js
+++ db/db_utils.js
@@ -1,14 +1,44 @@
+var ram = require('ramda')
+
 // Here we build the components that we string together to make a query
 
 
+var utils = module.exports;
 
 /*
-  return property of obj
+  return properties of obj in [obj]
 
   by (property, obj)
-    input:  obj
-    output: obj
+    input:   obj
+    output:  obj
+
+    // ramda
 */
+var by = function(property, objects) {
+  // convert non-array 'property' param to array
+  if (Array.isArray(property)) throw new Error('can not search for multiple properties with by. Use map(by) instead')
+  // Pass along object if we're not looking for anything
+  if (property.length === 0)    return objects
+
+  if (!Array.isArray(objects)) objects = [objects]
+
+  // console.log('\nproperty to pluck:', property)
+  // console.log('object to pluck from', objects)
+
+  var result = []
+  for (var obj of objects) {
+    // console.log('')
+    result.push( obj[property] )
+  }
+
+  return result;
+}
+
+      // c_id:    001,
+      // profile: { name: 'Dnin', age: 51, history: 'stringy stringy' },
+      // stats:   { hp: 32, prof: 3, level: 6 },
+      // items:   [ { _id: 0237, name: 'Gold', value: 1, quant: 89 },
+      //            { _id: 0181, name: 'Potion', value: 12, quant: 2 }]
 
 
 
@@ -19,5 +49,17 @@
   of (property, value)
     input: [obj]
     out:    obj
+
+    // ramda pickBy implementation
 */
 
+var trace = function(tag, x) {
+  console.log('')
+  console.log(tag, '<::>', x)
+  return x
+}
+
+
+utils.by = ram.curry(by)
+
+utils.trace = ram.curry(trace)
--- test/db_utils_spec.js
+++ test/db_utils_spec.js
@@ -1,14 +1,52 @@
+"use strict"
+
+var expect = require('chai').expect
+
 var DB    = require('../db/db')
 var utils = require('../db/db_utils')
+var ram   = require('ramda')
 
-describe('DB utility functions', funciton() {
+describe('DB utility functions', function() {
 
-  it ('should find an objects property', function() {
-
+  var Dnin = {
+    c_id:    1,
+    profile: { name: 'Dnin', age: 51, history: 'stringy stringy' },
+    stats:   { hp: 32, prof: 3, level: 6 },
+    items:   [ { _id: 237, name: 'Gold', value: 1, quant: 89 },
+               { _id: 181, name: 'Potion', value: 12, quant: 2 }]
   }
+  var Dangah = {
+    c_id:    2,
+    profile: { name: 'Dangah', age: 43, history: 'flingy thingy' },
+    stats:   { hp: 72, prof: 4, level: 13 },
+    items:   [ { _id: 237, name: 'Gold', value: 1, quant: 163 },
+               { _id: 181, name: 'Potion', value: 12, quant: 3 }]
+  }
+  var characters = [Dnin, Dangah]
 
+  describe('by', function() {
+    it ('should pluck properties from objects', function() {
+      let charName = utils.by('name', Dnin.profile)
 
+      expect(charName).to.deep.equal(['Dnin'])
+    })
 
+    it ('should pluck properties from an array of objects', function() {
+      let charNames = utils.by('c_id')(characters)
+      expect(charNames).to.deep.equal( [1, 2] )
+    })
 
+    it ('should compose', function() {
+      let characterName = ram.compose(utils.by('name'), utils.by('profile'))
+      let partyHealth = ram.compose(ram.sum, utils.by('hp'), utils.by('stats'))
 
-}
+      expect( characterName(Dangah) ).to.deep.equal(['Dangah'])
+      expect( characterName(Dnin) ).to.deep.equal(['Dnin'])
+
+      expect( characterName(characters) ).to.deep.equal(['Dnin', 'Dangah'])
+      expect( partyHealth(characters) ).to.equal(104)
+    })
+
+  })
+
+})
